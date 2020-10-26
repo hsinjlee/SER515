c711babbac972b502a3df3e80f222e743f8ae641
krh66 <krh66@asu.edu>
2020-10-26 14:57:49 -0700

    US#73Task#75 update scraping method

--- MapProject/Assets/Scripts/WebScraper.cs
+++ MapProject/Assets/Scripts/WebScraper.cs
@@ -20,8 +20,7 @@ public class WebScraper : MonoBehaviour
     public InputField password;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Debug.Log("start test on selenium");   
     }
 
@@ -29,14 +28,14 @@ public class WebScraper : MonoBehaviour
         IWebDriver driver = new ChromeDriver("/Users/Marcushsu/Documents/GitHub/ss/bin/Debug/netcoreapp3.0/");
         driver.Url = "https://weblogin.asu.edu/cas/login?service=https%3A%2F%2Fweblogin.asu.edu%2Fcgi-bin%2Fcas-login%3Fcallapp%3Dhttps%253A%252F%252Fwebapp4.asu.edu%252Fmyasu%252F%253Finit%253Dfalse";
         
-        DesiredCapabilities Usercapabilities = new DesiredCapabilities();
-        Usercapabilities.SetCapability("deviceName", "iPhone 8");
-        Usercapabilities.SetCapability("platformVersion", "10.3");
-        Usercapabilities.SetCapability("platformName", "iOS");
-        Usercapabilities.SetCapability("automationName", "XCUITest");
-        Usercapabilities.SetCapability("app", "/path/to/my.app");
-        Usercapabilities.SetCapability("MobileCapabilityType.APP", "System.getProperty('user.dir') + '/build/SampleiOS.app'");
-         //using the Appium_dot_net_driver (version 1.3.0.1) 
+        //DesiredCapabilities Usercapabilities = new DesiredCapabilities();
+        //Usercapabilities.SetCapability("deviceName", "iPhone 8");
+        //Usercapabilities.SetCapability("platformVersion", "10.3");
+        //Usercapabilities.SetCapability("platformName", "iOS");
+        //Usercapabilities.SetCapability("automationName", "XCUITest");
+        //Usercapabilities.SetCapability("app", "/path/to/my.app");
+        //Usercapabilities.SetCapability("MobileCapabilityType.APP", "System.getProperty('user.dir') + '/build/SampleiOS.app'");
+        //using the Appium_dot_net_driver (version 1.3.0.1) 
         
         //Initialise appium which throwing some error saying to add <IWebelement>
         //AppiumDriver<AppiumWebElement> driver1;
@@ -44,30 +43,38 @@ public class WebScraper : MonoBehaviour
         //driver1 = new IOSDriver<IWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), Usercapabilities);
         //driver1.Navigate().GoToUrl("https://weblogin.asu.edu/cas/login?service=https%3A%2F%2Fweblogin.asu.edu%2Fcgi-bin%2Fcas-login%3Fcallapp%3Dhttps%253A%252F%252Fwebapp4.asu.edu%252Fmyasu%252F%253Finit%253Dfalse"); //launch URL
 
-        IWebElement textbox1 = driver.FindElement(By.Id("username"));
-        textbox1.SendKeys(username.text);
+        IWebElement usernameTextbox = driver.FindElement(By.Id("username"));
+        usernameTextbox.SendKeys(username.text);
 
-        IWebElement textbox2 = driver.FindElement(By.Name("password"));
-        textbox2.SendKeys(password.text);
+        IWebElement passwordTextbox = driver.FindElement(By.Name("password"));
+        passwordTextbox.SendKeys(password.text);
 
-        IWebElement textbox3 = driver.FindElement(By.Name("submit"));
-        textbox3.Submit();
+        IWebElement submitTextBox = driver.FindElement(By.Name("submit"));
+        submitTextBox.Submit();
         //new webpage
-        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        IWebElement textbox4 = wait.Until(e => e.FindElement(By.XPath("/html/body/div/main/div/div[@id='myasu-main-container']/div[@class='myasu-content-container']/div[@class='column-container']/div[@class='column-container-row']/div[@class='content-column']/div[@class='splitcontentleft']/section[@class='box' and @data-box-order ='30']/div[@id='classes_content']/div[@class='tab-pane']/div[@id='2207_tab']/div[@class='box-padding']/div[@id='my_schedule_line']/a")));
-        //IWebElement textbox4 = wait.Until(e => e.FindElement(By.XPath("//div[@id='2207_tab']/div[@class='box-padding']/[@id='my_schedule_line']/a")));
-        Debug.Log(textbox4.Text);
-        textbox4.Click();
-        driver.Quit();
-        Debug.Log("End test on selenium");   
-        //
-        SceneManager.LoadScene("MapDemo"); 
-    }
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3600));
+        IWebElement scheduleTextbox = wait.Until(e => e.FindElement(By.XPath("//div[@id='2207_tab']/div[@class='box-padding']/div[@id='my_schedule_line']/a")));
+        Debug.Log(scheduleTextbox.Text);
+        scheduleTextbox.Click();
 
+        WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        IList<IWebElement> className = wait1.Until(e => e.FindElements(By.XPath("//td[@data-label='Course']")));
+        String[] classSchedules = new String[className.Count];
+        int i = 0;
+        foreach(IWebElement name in className) {
+            classSchedules[i++] = name.Text;
+            Debug.Log(name.Text);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        IList<IWebElement> classLocation = wait1.Until(e => e.FindElements(By.XPath("//td[@data-label='Location']")));
+        i = 0;
+        foreach(IWebElement loc in classLocation) {
+            classSchedules[i++] = loc.Text;
+            Debug.Log(loc.Text);
+        }
         
+        driver.Quit();
+        Debug.Log("End test on selenium");
+        SceneManager.LoadScene("MapDemo");
     }
 }
