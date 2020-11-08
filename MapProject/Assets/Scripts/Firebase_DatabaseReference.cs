329f8ced05d5a82464e8c70d9f86ffdc6d4a64d3
hsinjlee <rlee4408@gmail.com>
2020-11-08 00:34:06 -0700

    us#44task#92 apply firebase
    us#44task#92 apply firebase

--- MapProject/Assets/Scripts/Firebase_DatabaseReference.cs
+++ MapProject/Assets/Scripts/Firebase_DatabaseReference.cs
@@ -1,6 +1,7 @@
 ﻿using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
+//using Firebase
 //using Firebase.Storage.FirebaseStorage;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,19 +13,30 @@ public class Firebase_DatabaseReference : MonoBehaviour
     {
         Firebase.Storage.FirebaseStorage storage = Firebase.Storage.FirebaseStorage.DefaultInstance;
 
-        // Points to the root reference
+        //Points to the root reference
         Firebase.Storage.StorageReference storage_ref =
           storage.GetReferenceFromUrl("gs://mapproject-fb612.appspot.com");
 
+
         var new_metadata = new Firebase.Storage.MetadataChange
         {
-            LocationMetadata = new Dictionary<string, string> {
-                {"className", "Foundations of Software Engineering"},
-                {"classLocation", "Poly PICHO 150"}
+            CustomMetadata = new Dictionary<string, string> {
+                 {"className", "Foundations of Software Engineering"},
+                 {"classLocation", "Poly PICHO 150"}
             }
         };
 
-    // UpdateMetadataAsync
+        // UpdateMetadataAsync
+
+        //var new_metadata = new Firebase.Storage.MetadataChange
+        //{
+        //    LocationMetadata = new Dictionary<string, string> {
+        //        {"className", "Foundations of Software Engineering"},
+        //        {"classLocation", "Poly PICHO 150"}
+        //    }
+        //};
+
+        // UpdateMetadataAsync
 
         // Get the root reference location of the database.
         DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
