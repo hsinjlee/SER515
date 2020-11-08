45e5770177e0f53ee46985896db68f24afa24430
hsinjlee <rlee4408@gmail.com>
2020-11-07 17:25:28 -0700

    us#44task#92 apply firebase write
    us#44task#92 apply firebase write

---
+++ MapProject/Assets/Scripts/Firebase_classSchedules.cs
@@ -0,0 +1,21 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firebase_classSchedules : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
