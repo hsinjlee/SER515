d7c7c70ea13ade03de9574d51aac2388b0e4b7a5
Weixiang Zhang <weixiangz0609@gmail.com>
2020-11-23 19:31:56 -0700

    Task #108：Add functionality to Update Button in the sub-menu
    Hidden so far

--- MapProject/Assets/Scripts/ScheduleManager.cs
+++ MapProject/Assets/Scripts/ScheduleManager.cs
@@ -14,6 +14,7 @@ public class ScheduleManager : MonoBehaviour
 
     private List<List<string>> weekSchedule;
     private Dictionary<string, HashSet<string>> classDetails;
+    private bool updated = false;
 
 
     // Start is called before the first frame update
@@ -29,12 +30,16 @@ public class ScheduleManager : MonoBehaviour
             weekSchedule = webScraper.weekSchedule;
             classDetails = webScraper.classDetails;
         }
-        else {
+        else
+        {
             Debug.Log("Scraper Not Found.");
         }
-        
-        classSchedule = new List<string> { "Memorial Union", "Hayden Library", "ASU SDFC Field", "ASU ISTB 4" };
-       // GenerateSchedule();
+
+        // classSchedule = new List<string> { "Memorial Union", "Hayden Library", "ASU SDFC Field", "ASU ISTB 4" };
+        if (!updated)
+        {
+            GenerateSchedule();
+        }
         foreach (string s in classSchedule)
         {
             Debug.Log(s);
@@ -43,58 +48,100 @@ public class ScheduleManager : MonoBehaviour
         scrollView.verticalNormalizedPosition = 1;
     }
 
-    public void GenerateSchedule() {
-        DateTime today = DateTime.Today;
-        string weekday = today.ToString().Split(',')[0];
-        //int key;
-        //switch (weekday) {
-        //    case "Monday":
-        //        key = 0;
-        //        break;
-        //    case "Tuesday":
-        //        key = 1;
-        //        break;
-        //    case "Wednesday":
-        //        key = 2;
-        //        break;
-        //    case "Thursday":
-        //        key = 3;
-        //        break;
-        //    case "Friday":
-        //        key = 4;
-        //        break;
-        //    default:
-        //        return;
-        //}
-        List<string> dailyClass = weekSchedule[1];
+    public void GenerateSchedule()
+    {
+        string weekday = DateTime.Today.DayOfWeek.ToString();
+        int key;
+        switch (weekday)
+        {
+            case "Monday":
+                key = 0;
+                break;
+            case "Tuesday":
+                key = 1;
+                break;
+            case "Wednesday":
+                key = 2;
+                break;
+            case "Thursday":
+                key = 3;
+                break;
+            case "Friday":
+                key = 4;
+                break;
+            default:
+                return;
+        }
+        List<string> dailyClass = weekSchedule[key];
         foreach (string course in dailyClass)
         {
             Debug.Log(course);
             HashSet<string> details = classDetails[course];
-            
+
             string location = null;
             foreach (string l in details)
             {
-                Debug.Log(l);
                 location = l;
             }
-            classSchedule.Add(location);
+            if (!location.Equals("ASU Sync"))
+            {
+                classSchedule.Add(StringFilter(location));
+            }
+
         }
 
     }
 
-    public void BackToMainPage() {
+    public void BackToMainPage()
+    {
         SceneManager.LoadScene("MapDemo");
     }
 
-    void GenerateItem(string str) {
-        GameObject scrollItemObj = Instantiate (scrollItemPrefab);
+    void GenerateItem(string str)
+    {
+        GameObject scrollItemObj = Instantiate(scrollItemPrefab);
         scrollItemObj.transform.SetParent(scrollContent.transform, false);
         scrollItemObj.transform.Find("Placeholder").gameObject.GetComponent<Text>().text = str;
     }
 
-    public List<string> getClassSchedule() {
+    public List<string> getClassSchedule()
+    {
         return classSchedule;
+    }
+
+    public void UpdateButton()
+    {
+        List<string> updatedList = new List<string>();
+        int childCount = scrollContent.transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform scrollItem = scrollContent.transform.GetChild(i);
+            string newLocation = scrollItem.transform.Find("Text").gameObject.GetComponent<Text>().text;
+            if (newLocation.Length == 0)
+            {
+                newLocation = scrollItem.transform.Find("Placeholder").gameObject.GetComponent<Text>().text;
+            }
+            scrollItem.transform.Find("Placeholder").gameObject.GetComponent<Text>().text = newLocation;
+            updatedList.Add(StringFilter(newLocation));
+        }
+        classSchedule = updatedList;
+        updated = true;
+    }
+
+    private string StringFilter(string s) {
+        s = s.Replace("PRLTA", "PERALTA HALL");
+        s = s.Replace("SANTN", "SANTAN HALL");
+        s = s.Replace("AGBC", "AGRIBUSINESS CENTER");
+        s = s.Replace("SANCA", "SANTA CATALINA HALL");
+        s = s.Replace("TECH", "TECHNOLOGY CENTER");
+        s = s.Replace("ARAVA", "ARAVAIPA AUDITORIUM");
+        s = s.Replace("PICHO", "PICACHO HALL");
+        s = s.Replace("SIM", "SIMULATOR BUILDING");
+        s = s.Replace("ENGR", "ENGINEERING STUDIO");
+        s = s.Replace("LNTAHL", "LANTANA HALL");
+        s = s.Replace("CNTR", "ACADEMIC CENTER");
+        s = s.Replace("SDFCP", "SUN DEVIL FITNESS COMPLEX");
+        return s;
     }
 
     private void Awake()
